749c162d4fd00f9ecfaba68eae9edb43983918f6
Diksha Bajaj <diksha.bajaj@hpe.com>
2017-07-26 12:52:44 +0530

    Added code for start test

--- TestForSureUI/WebContent/js/start-test.js
+++ TestForSureUI/WebContent/js/start-test.js
@@ -1,8 +1,13 @@
+var allQuestions=[];
+var no_of_ques;
+var candidateResponse={};
+candidateResponse.testDetails={};
+candidateResponse.result=[];
 $('#btnProceed').on('click', function(){
 	$('#divInstructions').removeClass('show');
 	$('#divInstructions').addClass('hide');
-	//$('#divQuestions').removeClass('hide');
-	//$('#divQuestions').addClass('show');
+	$('#divQuestions').removeClass('hide');
+	$('#divQuestions').addClass('show');
 	var testId = ((localStorage.getItem('test_id')).split('-'))[1];
 	console.log("Test id: "+testId);
 	
@@ -21,7 +26,10 @@ $('#btnProceed').on('click', function(){
 							questionNumber += "<a href='#'><div style='float:left;border:solid 1px black;width:20%'>" + i + "</div></a>";
 						}
 						console.log(questionNumber);
-						$('#questionsPanel').append(questionNumber);
+						$('#questionNumber').append(questionNumber);
+						
+						$('#quesAttemptedCount').text('0/'+result.testDetails.no_of_ques);
+						no_of_ques = parseInt(result.testDetails.no_of_ques);
 					}
 					else if(!result.status){
 						console.log("Error: "+result.message);
@@ -31,33 +39,29 @@ $('#btnProceed').on('click', function(){
 					console.log("Error in getting questions");
                 }
             });
-	/*var getQuestions_url = "http://localhost:8083/test-for-sure/test/get-questions?test_id="+testId;
+			
+			
+	var getQuestions_url = "http://localhost:8083/test-for-sure/test/get-questions?test_id="+testId;
 	$.ajax({
-                url: url,
+                url: getQuestions_url,
                 type: "GET",
                 dataType: 'json',
                 success: function (result) {
 					if(result.status){
 						if(result.question != null) {
-							console.log(JSON.stringify(result.question));
+							//console.log(JSON.stringify(result.question));
 							$.each(result.question, function(i, question) {
-								//var btnId = 'btnTest-'+test.id;
-								//var query_string = 'test_id='+btnId;
-								var newQuestion = "<div style='border:solid 1px red;'>"+
-												"</br>Question Id: "+question.id+
-												"</br>Question Type: "+question.ques_type;
-								if((question.ques_type).toLowerCase() == "paragraph"){
-									newQuestion += "</br>Paragraph text: "+question.paragraph_text;
-								}
-												newQuestion += "</br>Question Text: "+question.ques_text+
-												"</br>a. "+question.optionA+
-												"</br>b. "+question.optionB+
-												"</br>c. "+question.optionC+
-												"</br>d. "+question.optionD+
-												"</div>";
-								//$('#divQuestions').append(newQuestion);
+								allQuestions.push(question);
 							});
-							
+							console.log(JSON.stringify(allQuestions));
+							$('#displayQuestion').append(questionStructure(result.question[0].paragraph_text, result.question[0].ques_text, result.question[0].optionA, result.question[0].optionB, result.question[0].optionC, result.question[0].optionD));
+							console.log("no_of_ques: "+no_of_ques);
+							if(no_of_ques == 1){
+								$('#btnSaveNext').html('Save');
+							}
+							else{
+								$('#btnSaveNext').html('Save & Next');
+							}
 						}
 					}
 					else if(!result.status){
@@ -67,9 +71,123 @@ $('#btnProceed').on('click', function(){
                 error: function () {
 					console.log("Error in getting questions");
                 }
-            });*/
+            });
+			
 })
 
+$('#btnViewAll').on('click', function(){
+	console.log("AllQuestions:"+JSON.stringify(allQuestions));
+
+})
+
+function questionStructure(paraText, quesText, optionA, optionB, optionC, optionD){
+	var question = "";
+	question+="<span>Question "+localStorage.getItem('questionCount')+".</span>"
+	if(paraText != ""){
+		question += "<span>"+paraText+"</span>";
+	}
+	question += "<span>"+quesText+"</span>";
+	var optionsRadioButton = "<input type='radio' id='optionsA' name='options' value='a' text='"+optionA+"'>"+optionA+"</input>"+
+							 "<input type='radio' id='optionsB' name='options' value='b' text='"+optionB+"'>"+optionB+"</input>"+
+							 "<input type='radio' id='optionsC' name='options' value='c' text='"+optionC+"'>"+optionC+"</input>"+
+							 "<input type='radio' id='optionsD' name='options' value='d' text='"+optionD+"'>"+optionD+"</input>";
+	
+	question+=optionsRadioButton;
+	return(question);
+}
+
+$('#btnClearSelection').on('click', function(){
+	$('#displayQuestion input').removeAttr('checked');
+})
+
+$('#btnSaveNext').on('click', function(){
+	
+	
+	//getting the candidate's response
+	var response = {};
+	var exist = false;
+	var ques_id = allQuestions[parseInt(localStorage.getItem('questionCount'))-1].id;
+	var option_selected = $('input[name=options]:checked').val();
+	response.question_id = ques_id;
+	response.marked_option = option_selected;
+	for(var i = 0;i<(candidateResponse.result).length;i++){
+		if(candidateResponse.result[i].question_id == ques_id){
+			candidateResponse.result[i].marked_option = option_selected;
+			exist=true;
+			break;
+		}
+	}
+	if(!exist){
+		(candidateResponse.result).push(response);
+	}
+	//Count number of questions attempted
+	var count = 0;
+	for(var i = 0;i<(candidateResponse.result).length;i++){
+		if(candidateResponse.result[i].marked_option){
+			count++;
+		}
+	}
+	console.log("Candidate Response: "+JSON.stringify(candidateResponse));
+	console.log($('#btnSaveNext').html());
+	console.log("Questions attempted: "+count);
+	$('#quesAttemptedCount').text(count+'/'+no_of_ques);
+	var num;
+	
+	if($('#btnSaveNext').html() == 'Save &amp; Next'){
+		localStorage.setItem('questionCount', parseInt(localStorage.getItem('questionCount'))+1);
+		num = parseInt(localStorage.getItem('questionCount'))-1;
+		$('#displayQuestion').empty();
+		$('#displayQuestion').append(questionStructure(allQuestions[num].paragraph_text, allQuestions[num].ques_text, allQuestions[num].optionA, allQuestions[num].optionB, allQuestions[num].optionC, allQuestions[num].optionD ))
+		//Selecting the already selected response
+		var ques_id = allQuestions[num].id;
+		for(var i = 0;i<candidateResponse.result.length;i++){
+			if(candidateResponse.result[i].question_id == ques_id){
+				$('input[value="'+candidateResponse.result[i].marked_option+'"]').prop("checked", true);
+				break;
+			}
+		}
+		if((num+1)>1){
+			$('#btnPreviousQuestion').attr('disabled', false);
+		}
+		else{
+			$('#btnPreviousQuestion').attr('disabled', true);
+		}
+		if((num+1) == no_of_ques){
+			$('#btnSaveNext').html('Save');
+		}
+		else{
+			$('#btnSaveNext').html('Save & Next');
+		}
+	}
+})
+$('#btnPreviousQuestion').on('click', function(){
+	localStorage.setItem('questionCount', parseInt(localStorage.getItem('questionCount'))-1);
+	$('#displayQuestion').empty();
+	var num = parseInt(localStorage.getItem('questionCount'))-1;
+	$('#displayQuestion').append(questionStructure(allQuestions[num].paragraph_text, allQuestions[num].ques_text, allQuestions[num].optionA, allQuestions[num].optionB, allQuestions[num].optionC, allQuestions[num].optionD ))
+	//Selecting the already selected response
+	var ques_id = allQuestions[num].id;
+	for(var i = 0;i<candidateResponse.result.length;i++){
+		if(candidateResponse.result[i].question_id == ques_id){
+			console.log('Inside matched question_id');
+			$('input[value="'+candidateResponse.result[i].marked_option+'"]').prop("checked", true);
+			break;
+		}
+	}
+	
+	if((num+1)>1){
+		$('#btnPreviousQuestion').attr('disabled', false);
+	}
+	else{
+		$('#btnPreviousQuestion').attr('disabled', true);
+	}
+	if((num+1) == no_of_ques){
+		$('#btnSaveNext').html('Save');
+	}
+	else{
+		$('#btnSaveNext').html('Save & Next');
+	}
+})
 function getQueryParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -84,7 +202,9 @@ $(document).ready(function () {
 	console.log("Document start-test is ready");
 	var test_id = getQueryParameterByName('test_id');
 	localStorage.setItem('test_id', test_id);
+	localStorage.setItem('questionCount', 1);
 	console.log("Test id: "+test_id);
+	candidateResponse.testDetails.test_id = (test_id.split('-'))[1];
 	
 	
 })
