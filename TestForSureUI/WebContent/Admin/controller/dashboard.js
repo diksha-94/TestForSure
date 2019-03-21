dc77f5f336078d0c55703039b29096fbf2659d01
diksha-94 <bajaj.diksha45@gmail.com>
2019-03-21 23:59:45 +0530

    Admin login

--- TestForSureUI/WebContent/Admin/controller/dashboard.js
+++ TestForSureUI/WebContent/Admin/controller/dashboard.js
@@ -1,4 +1,5 @@
 var dashboardController = function(){
+	this.userId = "";
 	this.Init();
 };
 dashboardController.prototype.Init = function()
@@ -9,6 +10,18 @@ dashboardController.prototype.Init = function()
 			'<img src="../../Images/admin_user.jpg" alt="Admin"/>');
 			this.BindEvents();
 			new menuController($('.admin-menu'));
+			this.userId = getCookie('test2bsure_admin');
+			if(this.userId){
+				//Already login
+				$('.login-box').css('display', 'none');
+				$('.page-header').find('.admin-profile').css('display', 'block');
+				$('.after-login').css('display', 'block');
+			}
+			else{
+				$('.login-box').css('display', 'block');
+				$('.page-header').find('.admin-profile').css('display', 'none');
+				$('.after-login').css('display', 'none');
+			}
 		}.bind(this));
 	}.bind(this));
 };
@@ -26,9 +39,62 @@ dashboardController.prototype.BindEvents = function()
 		$('.admin-profile-box').find('#btnChangePass').unbind().bind('click', function(){
 			
 		});
-		$('.admin-profile-box').find('#btnLogout').unbind().bind('click', function(){
-			//destroy the session
-			window.location.href = 'index.html';
-		});
 	});
+	$('#btnLogin').unbind().bind('click', function(){
+		$('.error-message').empty();
+		var email = $('#txtEmail').val();
+		var password = $('#passPassword').val();
+		if(email == '' || password == ''){
+			$('.error-message').html('Email or password cannot be empty');
+			return;
+		}
+		if(!validateEmail(email)){
+			$('.error-message').html('Email is not valid');
+			return;
+		}
+		if(password.length < 6){
+			$('.error-message').html('Password cannot be smaller than 6 characters');
+			return;
+		}
+		this.Login(email, password);
+	}.bind(this));
+	$('#btnLogout').unbind().bind('click', function(){
+		this.Logout();
+	}.bind(this));
 };
+dashboardController.prototype.Login = function(email, password)
+{
+	var url = 'http://localhost:8083/test2bsure/authenticate-user'
+	var type = 'POST';
+	var requestData = {
+		"email": email,
+		"password": password
+	};
+	$.ajax({
+		url: url,
+		type: type,
+		contentType: "application/json",
+		data: JSON.stringify(requestData),
+		success: function(response){
+			console.log(response);
+			if(response.result.status == true){
+				setCookie("test2bsure_admin", response.userId, 1);
+				window.location.reload();
+			}
+			else{
+				$('.error-message').html(response.result.message);
+			}
+			
+		},
+		error: function(e){
+			console.log(e);
+		}
+	});
+}
+dashboardController.prototype.Logout = function()
+{
+	this.userId = "";
+	//remove the cookie
+	setCookie("test2bsure_admin", "", -1);
+	window.location.reload();
+}
--- TestForSureUI/WebContent/Portal/ReusableFunctions/test2bsure.js
+++ TestForSureUI/WebContent/Portal/ReusableFunctions/test2bsure.js
@@ -181,3 +181,26 @@ test2bsureController.prototype.Draw3DPieChart = function(valueObject, total, dom
     json.series = series;   
     $(dom).highcharts(json);
 }
+test2bsureController.prototype.SetCookie = function(cname, cvalue, exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+};
+test2bsureController.prototype.getCookie = function(cname)
+{
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
