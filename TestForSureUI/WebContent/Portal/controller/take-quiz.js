8278afe2f3e3be0b27992f390efeb1fbc5675ac5
diksha-94 <bajaj.diksha45@gmail.com>
2020-07-28 01:22:20 +0530

    Added favicon in all pages

--- TestForSureUI/WebContent/Portal/controller/take-quiz.js
+++ TestForSureUI/WebContent/Portal/controller/take-quiz.js
@@ -95,7 +95,14 @@ quizController.prototype.PopulateQuestion = function(solution)
 			redOption = marked;
 		}
 	}
-	$(question.options).find('option').each(function(key, value){
+	var options = question.options;
+	if(options.startsWith('"')){
+		options = options.substring(1);
+	}
+	if(options.endsWith('"')){
+		options = options.substring(0, options.length-1);
+	}
+	$(options).find('option').each(function(key, value){
 		var addClass = 'option';
 		if(key == redOption){
 			addClass = 'option incorrect';
