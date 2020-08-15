bf5e22a66365b99f569d069984ca44ce72bbaab0
diksha-94 <bajaj.diksha45@gmail.com>
2020-08-15 22:14:50 +0530

    exam banner css in mobile view

--- TestForSureUI/WebContent/Portal/ReusableFunctions/test2bsure.js
+++ TestForSureUI/WebContent/Portal/ReusableFunctions/test2bsure.js
@@ -1,9 +1,9 @@
-var jsVersion = 1;
-var cssVersion = 1;
+var jsVersion = 2;
+var cssVersion = 2;
 
 var obj = null;
-//var remoteServer = "http://3.6.58.203:8083";
-var remoteServer = "http://localhost:8083";
+var remoteServer = "http://3.6.58.203:8083";
+//var remoteServer = "http://localhost:8083";
 var test2bsureController = function(){
 	this.userData = null;
 };
