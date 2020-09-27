89a216ec893f6c59373ca5b68352c56392fe2d0b
diksha-94 <bajaj.diksha45@gmail.com>
2020-09-27 23:36:04 +0530

    Portal version mgmt, some minor fixes

---
+++ TestForSureUI/WebContent/Portal/ReusableFunctions/framework.js
@@ -0,0 +1,40 @@
+var jsVersion = 21;
+var cssVersion = 21;
+//var remoteServer = "http://3.6.58.203:8083";
+var remoteServer = "http://localhost:8083";
+
+function LoadCSS(filename) {
+	var id = "CSS_" + filename.replace(/[^a-zA-Z0-9_]+/, "_").replace(".", "_");
+	if (!document.getElementById(id)) {
+		link = document.createElement('link');
+		link.id = id;
+		link.type = 'text/css';
+		link.rel = 'stylesheet';
+		link.href = filename + ".css?v="+cssVersion;
+		document.head.appendChild(link);
+	}
+};
+
+function LoadJS(src, callback) {
+	var id = "JS_" + src.replace(/[^a-zA-Z0-9_]+/, "_").replace(".", "_");
+	if (!document.getElementById(id)) {
+		var s = document.createElement('script');
+		s.id = id;
+		s.src =  src + ".js?v="+jsVersion;
+		s.async = true;
+		s.onreadystatechange = s.onload = function() {
+			if ( typeof this.callback != 'undefined') {
+				this.callback();
+			}
+		}.bind({
+			obj : s,
+			callback : callback
+		});
+		document.getElementsByTagName('head')[0].appendChild(s);
+	} else {
+		if ( typeof callback !== "undefined") {
+			callback.done = true;
+			callback();
+		}
+	}
+};
--- TestForSureUI/WebContent/Portal/ReusableFunctions/test2bsure.js
+++ TestForSureUI/WebContent/Portal/ReusableFunctions/test2bsure.js
@@ -1,9 +1,4 @@
-var jsVersion = 21;
-var cssVersion = 21;
-
 var obj = null;
-//var remoteServer = "http://3.6.58.203:8083";
-var remoteServer = "http://localhost:8083";
 var test2bsureController = function(){
 	this.userData = null;
 };
@@ -406,41 +401,6 @@ test2bsureController.prototype.getCookie = function(cname)
         }
     }
     return "";
-};
-function LoadCSS(filename) {
-	var id = "CSS_" + filename.replace(/[^a-zA-Z0-9_]+/, "_").replace(".", "_");
-	if (!document.getElementById(id)) {
-		link = document.createElement('link');
-		link.id = id;
-		link.type = 'text/css';
-		link.rel = 'stylesheet';
-		link.href = filename + ".css?v="+cssVersion;
-		document.head.appendChild(link);
-	}
-};
-
-function LoadJS(src, callback) {
-	var id = "JS_" + src.replace(/[^a-zA-Z0-9_]+/, "_").replace(".", "_");
-	if (!document.getElementById(id)) {
-		var s = document.createElement('script');
-		s.id = id;
-		s.src =  src + ".js?v="+jsVersion;
-		s.async = true;
-		s.onreadystatechange = s.onload = function() {
-			if ( typeof this.callback != 'undefined') {
-				this.callback();
-			}
-		}.bind({
-			obj : s,
-			callback : callback
-		});
-		document.getElementsByTagName('head')[0].appendChild(s);
-	} else {
-		if ( typeof callback !== "undefined") {
-			callback.done = true;
-			callback();
-		}
-	}
 };
 test2bsureController.prototype.SelfAuth = function(callback)
 {
