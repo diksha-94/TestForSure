e8fc5506317203e411b3fae7f1ba0d615da78774
diksha-94 <bajaj.diksha45@gmail.com>
2020-10-16 02:00:29 +0530

    added constants

---
+++ TestForSureUI/WebContent/Portal/ReusableFunctions/Constants.js
@@ -0,0 +1,38 @@
+//Defines the mapping - pageType in DB to page name to open
+var pageMapping = {
+		//"pageType in DB"  : "page to open"
+		"home"				: "default",
+		"exam"				: "default",
+		"quizsubject"		: "quiz",
+		"test"				: "taketest",
+		"quiz"				: "takequiz",
+		"aboutus"			: "default",
+		"contactus"			: "default",
+		"disclaimer"		: "disclaimer",
+		"emailverification"	: "EmailVerification",
+		"passwordreset"		: "password-reset",
+		"privacy"			: "default"
+}
+var headermenu = [
+                  "home", 
+                  "exam", 
+                  "quizsubject", 
+                  "aboutus", 
+                  "contactus", 
+                  "disclaimer",
+                  "emailVerification",
+                  "passwordreset",
+                  "privacy"
+                  ];
+
+var footercontent = [
+                     "home", 
+                     "exam", 
+                     "quizsubject", 
+                     "aboutus", 
+                     "contactus", 
+                     "disclaimer",
+                     "emailVerification",
+                     "passwordreset",
+                     "privacy"
+                     ];
